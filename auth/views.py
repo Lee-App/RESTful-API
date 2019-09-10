be8edca90b46cda5620008eaa4df23231a4fec5d
Lee-App <asow7345@hallym.ac.kr>
2019-09-10 18:14:34 +0900

    User test code

--- auth/views.py
+++ auth/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth.models import User
 
-# Create your views here.
+from .serializers import UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
