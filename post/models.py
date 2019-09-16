34e4f960c15c3312d01fe6c34038ff7fb0c76ff0
Lee-App <asow7345@hallym.ac.kr>
2019-09-16 23:36:14 +0900

    Add Model Comment

--- post/models.py
+++ post/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 
 class Post(models.Model):
+    ''' Post Model '''
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=128, null=False)
     context = models.TextField()
@@ -10,6 +11,7 @@ class Post(models.Model):
     updated_date = models.DateField(auto_now_add=True)
 
 class Comment(models.Model):
+    ''' Post Comment Model '''
     post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     context = models.TextField()
